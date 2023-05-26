8537c82452df0eddefbb5a3a07877d92ca0768d5
KoulteghDevOps <koultegh@outlook.com>
2023-05-26 22:15:24 +0100

    my first commit

---
+++ env-dev/state.tfvars
@@ -0,0 +1,3 @@
+bucket = "roboshop-project"
+key = "roboshop/prod/terraform.tfstate"
+region = "us-east-1"
---
+++ main.tf
@@ -0,0 +1,6 @@
+module "vpc" {
+  source = "git::https://github.com/KoulteghDevOps/tf_module_vpc.git"
+  
+  for_each = var.vpc
+  cidr_block = each.value["cidr_block"]
+}
