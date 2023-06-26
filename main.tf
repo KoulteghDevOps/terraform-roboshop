7ce89214d71bc7b53175f5543653433d489a80a0
KoulteghdevOps <koultegh@outlook.com>
2023-06-26 09:21:11 +0100

    manifest pushed

--- main.tf
+++ main.tf
@@ -145,6 +145,7 @@ module "vpc" {
 module "eks" {
   source             = "github.com/r-devops/tf-module-eks"
   ENV                = var.env
+  VERSION            = 1.27
   PRIVATE_SUBNET_IDS = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "application", null), "subnet_ids", null)
   PUBLIC_SUBNET_IDS  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "public", null), "subnet_ids", null)
   DESIRED_SIZE       = 2
